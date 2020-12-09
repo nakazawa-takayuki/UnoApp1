ab0365e5fef60f7fb044e9dda4caa3314a2e2cb9
nakazawa takayuki <nakazawa-takayuki@caica.jp>
2020-12-09 15:40:23 +0900

    リストクリック時後、画面遷移実装

--- UnoApp1.Shared/MainPage.xaml.cs
+++ UnoApp1.Shared/MainPage.xaml.cs
@@ -1,26 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using UnoApp1.Shared;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using UnoApp1.Shared;
 
 namespace UnoApp1
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
@@ -34,6 +17,20 @@ namespace UnoApp1
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
             textBlock1.Text = $"{textBox1.Text}{textBox2.Text}";
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var view = sender as ListView;
+            var index = view.SelectedIndex;
+            if (index < 0) return;
+            var value = (view.DataContext as string[])[index];
+
+            this.Frame.Navigate(typeof(SecondPage), value);
+
+            var data = listView.DataContext;
+            listView.DataContext = null;
+            listView.DataContext = data;
         }
     }
 }
---
+++ UnoApp1.Shared/SecondPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace UnoApp1.Shared
+{
+    public sealed partial class SecondPage : Page
+    {
+        public SecondPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click_GoBack(object sender, RoutedEventArgs e)
+        {
+            this.Frame.GoBack();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            textBlock.Text = e.Parameter as string;
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
